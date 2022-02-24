eb5cfaf24e12e29b6438dbda04a6a205bf65a8aa
Frenk <dragarfrenk@gmail.com>
2022-02-24 19:56:01 +0100

    ADD io and to_str to models

--- io.py
+++ io.py
@@ -1,44 +1,66 @@
+from models import Skill, Contributer, Project
+
+
 def read_inputs(filename="input_data/a_an_example.in.txt"):
     """Reads the inputs and instantiates objects"""
     f = open(filename, "r")
     num_contributors, num_projects = map(int, f.readline().split(" "))
-    
+
     # Create contributors
     contributors = []
     for i in range(num_contributors):
         contributor_params = f.readline().split(" ")
-        name = contributor_params[0]
+        contributor_name = contributor_params[0]
         num_skills = int(contributor_params[1])
 
+        skills = []
         for j in range(num_skills):
             skill_params = f.readline().split(" ")
-            skill = skill_params[0]
+            skill_name = skill_params[0]
             skill_level = int(skill_params[1])
+            skill = Skill(skill_name, skill_level)
+            skills.append(skill)
 
-        #contributors.append()
-    
+        contributer = Contributer(contributor_name, skills)
+        contributors.append(contributer)
+
+    # Create projects
+    projects = []
+    for i in range(num_projects):
+        project_name, di, si, bi, ri = f.readline().split(" ")
+        n_roles = int(ri)
+        needs_days = int(di)
+        best_before = int(bi)
+        score_worth = int(si)
+
+        # Roles in project
+        skills = []
+        for j in range(n_roles):
+            skill_params = f.readline().split(" ")
+            skill_name = skill_params[0]
+            skill_level = int(skill_params[1])
+            skill = Skill(skill_name, skill_level)
+            skills.append(skill)
+
+        project = Project(project_name, skills, needs_days,
+                          best_before, score_worth, n_roles)
+        projects.append(project)
+
+    f.close()
+
+    return contributors, projects
+
+
+def write_outputs(output, filename="output_data/output.txt"):
+    """Writes the outputs into the correct format"""
+    f = open(filename, "w")
+    f.write(output)
     f.close()
 
 
-def write_outputs(filename="input_data/a_an_example.in.txt"):
-    """Writes the outputs into the correct format"""
-    f = open(filename, "r")
-    num_contributors, num_projects = map(int, f.readline().split(" "))
-    
-    # Create contributors
-    contributors = []
-    for i in range(num_contributors):
-        contributor_params = f.readline().split(" ")
-        name = contributor_params[0]
-        num_skills = int(contributor_params[1])
-
-        for j in range(num_skills):
-            skill_params = f.readline().split(" ")
-            skill = skill_params[0]
-            skill_level = int(skill_params[1])
-
-        #contributors.append()
-
-
 if __name__ == '__main__':
-    read_inputs()
+    c, p = read_inputs()
+    for i in c:
+        print(i)
+    for i in p:
+        print(i)
--- models.py
+++ models.py
@@ -6,12 +6,19 @@ class Skill:
         self.name = name
         self.level = level
 
+    def __str__(self) -> str:
+        return f"{self.name}-{self.level}"
+
 
 class Contributer:
     def __init__(self, name: str, skills: List[Skill]) -> None:
         self.name = name
         self.skills = skills
         self.free_after = 0
+
+    def __str__(self) -> str:
+        skills_string = ', '.join([str(x) for x in self.skills])
+        return f"{self.name}: {skills_string}"
 
 
 class Project:
@@ -22,3 +29,6 @@ class Project:
         self.needs_days = needs_days
         self.score_worth = score_worth
         self.n_roles = n_roles
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.n_roles}"
